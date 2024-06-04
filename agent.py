b5f22a8e19d90a99f826a0e49cec761adf143689
zyj <edenzyj88@gmail.com>
2024-06-04 20:05:49 +0800

    final version

--- agent.py
+++ agent.py
@@ -24,18 +24,18 @@ class Agent:
         self.memory = deque(maxlen=MAX_MEMORY) # popleft()
         self.reward = 0
         
-        # Q-Learning TD
+        '''# Q-Learning TD
         self.alpha = 0.1
         self.epsilon_min = 0.01
         self.epsilon_decay = 0.995
-        self.q_table = {}
+        self.q_table = {}'''
         
         # Q-Learning tensorflow
         #self.model = self.network()
         
         # DQN
-        #self.model = Linear_QNet(11, 256, 3)
-        #self.trainer = QTrainer(self.model, lr=LR, gamma=self.gamma)
+        self.model = Linear_QNet(11, 256, 3)
+        self.trainer = QTrainer(self.model, lr=LR, gamma=self.gamma)
 
     def decay_epsilon(self):
         if self.epsilon > self.epsilon_min:
@@ -151,7 +151,7 @@ class Agent:
         else:
             mini_sample = self.memory
         
-        # Q-Learning tensorflow
+        '''# Q-Learning tensorflow
         for state, action, reward, next_state, done in mini_sample:
             target = reward
             if not done:
@@ -162,10 +162,10 @@ class Agent:
         '''
         # DQN
         states, actions, rewards, next_states, dones = zip(*mini_sample)
-        self.trainer.train_step(states, actions, rewards, next_states, dones)'''
+        self.trainer.train_step(states, actions, rewards, next_states, dones)
         
     def train_short_memory(self, state, action, reward, next_state, done):
-        # Q-Learning tensorflow
+        '''# Q-Learning tensorflow
         target = reward
         if not done:
             target = reward + self.gamma * np.amax(self.model.predict(np.array([next_state]))[0])
@@ -174,14 +174,14 @@ class Agent:
         self.model.fit(np.array([state]), target_f, epochs=1, verbose=0)
         '''
         # DQN
-        self.trainer.train_step(state, action, reward, next_state, done)'''
+        self.trainer.train_step(state, action, reward, next_state, done)
 
     def get_action(self, state):
         # random moves: tradeoff exploration / exploitation
         
         final_move = [0,0,0]
         
-        # Q-Learning TD
+        '''# Q-Learning TD
         if random.uniform(0, 1) < self.epsilon:
             move = random.randint(0, 2)
             final_move[move] = 1
@@ -189,14 +189,14 @@ class Agent:
             q_values = [self.get_q_value(state, a) for a in range(3)]
             move = np.argmax(q_values)
             final_move[move] = 1
-            
-        '''
+        '''    
+        
         self.epsilon = 80 - self.n_games
         if random.randint(0, 200) < self.epsilon:
             move = random.randint(0, 2)
             final_move[move] = 1
         else:
-            # Q-Learning tensorflow
+            '''# Q-Learning tensorflow
             state0 = tf.convert_to_tensor(state, dtype=tf.float32)
             state0 = state0[tf.newaxis, :]
             prediction = self.model.predict(state0)[0]
@@ -204,13 +204,13 @@ class Agent:
             for i in range(2):
                 if prediction[i] > prediction[move]:
                     move = i
-            
+            '''    
             # DQN
             state0 = torch.tensor(state, dtype=torch.float)
             prediction = self.model(state0)
             move = torch.argmax(prediction).item()
             
-            final_move[move] = 1'''
+            final_move[move] = 1
 
         return final_move
 
@@ -236,10 +236,10 @@ def train():
             state_new = agent.get_state(game)
 
             # update short time q-value of TD method
-            agent.update_short_q_value(state_old, final_move, reward, state_new, done)
+            #agent.update_short_q_value(state_old, final_move, reward, state_new, done)
             
             # train short memory of other methods
-            #agent.train_short_memory(state_old, final_move, reward, state_new, done)
+            agent.train_short_memory(state_old, final_move, reward, state_new, done)
 
             # remember
             agent.remember(state_old, final_move, reward, state_new, done)
@@ -250,15 +250,15 @@ def train():
                 agent.n_games += 1
                 
                 # update long time q-value of TD method 
-                agent.update_long_q_value()
+                #agent.update_long_q_value()
                 
                 # train long memory of other methods
-                #agent.train_long_memory()
+                agent.train_long_memory()
 
                 if score > record:
                     record = score
                     # save model of other methods
-                    #agent.model.save()
+                    agent.model.save()
 
                 print('Game', agent.n_games, 'Score', score, 'Record:', record)
 
@@ -269,7 +269,7 @@ def train():
                 plot(plot_scores, plot_mean_scores)
                 
                 # Q-Learning TD
-                agent.decay_epsilon()
+                #agent.decay_epsilon()
                 
         except KeyboardInterrupt:
             time.sleep(10)
